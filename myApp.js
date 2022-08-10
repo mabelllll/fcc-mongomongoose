3bc9a11a6c0c12c8d190990c6231ffbf2010f60c
Mabel Lee <leemabel.711@gmail.com>
2022-08-11 00:35:42 +0200

    delete many documents with model.remove()

--- myApp.js
+++ myApp.js
@@ -95,8 +95,9 @@ const removeById = (personId, done) => {
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
-
-  done(null /*, data*/);
+  Person.remove({name: nameToRemove}, (err, data) => {
+    err ? console.error(err) : done(null, data)
+  })
 };
 
 const queryChain = (done) => {
